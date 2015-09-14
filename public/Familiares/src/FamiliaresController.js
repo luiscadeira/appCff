d07068098e46b2407fab2c15d97c611cf4551cba
Tiago Alves <tcristianoalves@outlook.com>
2015-09-14 00:57:05 -0300

    Adicionado nome da familia a tabela

--- public/Familiares/src/FamiliaresController.js
+++ public/Familiares/src/FamiliaresController.js
@@ -1,11 +1,13 @@
-app.controller('FamiliaresCtrl', ['$scope', 'FamiliaresService'
-	function ($scope,FamiliaresService) {
+app.controller('FamiliaresCtrl', ['$scope','StorageData', 'FamiliaresService',
+	function ($scope,StorageData,FamiliaresService) {
 
     $scope.familiares = null;
 		
     if(StorageData.getFamilia()) {
       FamiliaresService.query(function(data) {
           $scope.familiares = data._embedded.familiares;
+          $scope.familia = data._embedded.familiares[0].familia.nome;
+          de($scope.familia)
       });
     } 
 	
