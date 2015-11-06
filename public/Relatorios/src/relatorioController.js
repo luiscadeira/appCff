ed9786417c25ed35de590ee788da41848636fcdf
Tiago Cristiano alves <tcristinoalves@outlook.com>
2015-11-05 23:50:38 -0200

    Orcamento

--- public/Relatorios/src/relatorioController.js
+++ public/Relatorios/src/relatorioController.js
@@ -1,1 +1,17 @@
+(function(){
+	'use strict';
+	angular.module('app')
+		.controller('RelatorioCtrl',RelatorioCtrl );
 
+		RelatorioCtrl.$injet = ['$scope'];
+
+		function RelatorioCtrl ($scope){
+
+			$scope.consultar = function(data){
+				de($scope.data);
+				de('teste');
+			}
+
+		};
+
+})()
