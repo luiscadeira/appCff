4247b46987dfa3f97f76af4dd27f87b3d13852fb
Tiago Cristiano alves <tcristinoalves@outlook.com>
2015-11-07 14:18:21 -0200

    Orcamentos

---
+++ public/Orcamento/src/orcamentoController.js
@@ -0,0 +1,10 @@
+(function(){
+	'use strict';
+	 angular.module('app')
+	 	.controller('OrcamentoCtrl', OrcamentoCtrl);
+	 	OrcamentoCtrl.$inject = ['$scope', 'OrcamentoService'];
+	 	function OrcamentoCtrl($scope,OrcamentoService){
+
+	 	}
+
+})();
---
+++ public/Orcamento/src/orcamentoService.js
@@ -0,0 +1,9 @@
+(function(){
+	'use strict';
+	angular.module('app')
+		.factory('OrcamentoService',OrcamentoService);
+		OrcamentoService.$inject = ['$resource'];
+		function OrcamentoService($resource) {
+
+		}
+})();
