95363e2ab16c228082d957d8bec6fc6d88fe6db5
Tiago Cristiano Alves <tcristianoalves@outlook.com>
2015-11-10 18:52:21 -0300

    orcamentos

--- public/Orcamento/src/orcamentoController.js
+++ public/Orcamento/src/orcamentoController.js
@@ -2,8 +2,19 @@
 	'use strict';
 	 angular.module('app')
 	 	.controller('OrcamentoCtrl', OrcamentoCtrl);
-	 	OrcamentoCtrl.$inject = ['$scope', 'OrcamentoService'];
-	 	function OrcamentoCtrl($scope,OrcamentoService){
+	 	OrcamentoCtrl.$inject = ['$scope', 'OrcamentoService','$location','Notification','StorageData'];
+
+	 	function OrcamentoCtrl($scope,OrcamentoService,$location,Notification,StorageData){
+
+            if(StorageData.getFamilia() && $location.path() != '/newOrcamento') {
+                OrcamentoService.query().$promise.then(
+                function(success) {
+                    $scope.orcamentosList = success._embedded.orcamentos;
+                },
+                function( error ){
+                    Notification.error({message: error.statusText , delay: 9000});
+                });
+            }
 
 	 	}
 
--- public/Orcamento/src/orcamentoService.js
+++ public/Orcamento/src/orcamentoService.js
@@ -1,9 +1,32 @@
 (function(){
-	'use strict';
-	angular.module('app')
-		.factory('OrcamentoService',OrcamentoService);
-		OrcamentoService.$inject = ['$resource'];
-		function OrcamentoService($resource) {
 
-		}
+'use strict';
+
+app.factory('OrcamentoService',['$resource', 'StorageData',
+  function ($resource,StorageData) {
+
+  var id_familia = StorageData.getFamilia();
+   
+  return $resource(BASE_URL + "/orcamentos/:id", {}, {
+
+    query: { 
+      method : "GET",
+      isArray: false ,
+      url : BASE_URL + "/orcamentos?familia_id="+id_familia,
+      params: {id_familia : '@id_familia'}
+    },
+
+    create: { method: 'POST' },
+    'delete': {
+        method:'DELETE'
+    },
+
+    update: { method: 'PUT', params: {id: '@id'} },
+
+    show: { method: 'GET' },
+  });
+    
+
+}]);
+
 })();
