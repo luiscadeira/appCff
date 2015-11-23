a09a36dee1290fd51b26f209ff057231a9a0dfa6
Tiago Cristiano Alves <tcristianoalves@outlook.com>
2015-11-23 16:47:52 -0300

    tentantivas de gerar os graficos

--- public/Relatorios/src/relatorioController.js
+++ public/Relatorios/src/relatorioController.js
@@ -1,52 +1,54 @@
-(function(){
-	'use strict';
-	angular.module('app')
-		.controller('RelatorioCtrl',RelatorioCtrl );
+(function() {
+  'use strict';
+  angular.module('app')
+    .controller('RelatorioCtrl', RelatorioCtrl);
 
-		RelatorioCtrl.$injet = ['$scope', 'StorageData', '$location','$http'];
+  RelatorioCtrl.$injet = ['$scope', 'StorageData', '$location', '$http'];
 
-		function RelatorioCtrl ($scope,StorageData, $location,$http){
+  function RelatorioCtrl($scope, StorageData, $location, $http) {
 
-			$scope.showCarts = false;
+    $scope.showCarts = false;
 
-			$scope.consultar = function(){
+    $scope.consultar = function() {
 
-				 var id_familia = StorageData.getFamilia();
+      var id_familia = StorageData.getFamilia();
 
-				
-				 $scope.dataDe       = new Date($scope.dataDe);
-			     var dateDe          = $scope.dataDe.toLocaleString();
-			     dateDe = dateDe.substring(0, 10);		    
-				 $scope.dataAte     = new Date($scope.dataAte);
-			     var dateAte        = $scope.dataAte.toLocaleString();
-			     dateAte = dateAte.substring(0, 10);
 
-			     var consulta = {
-			        de : dateDe,
-			        ate: dateAte,
-			        tipo : $scope.consulta.tipo
-			     };
+      $scope.dataDe = new Date($scope.dataDe);
+      var dateDe = $scope.dataDe.toLocaleString();
+      dateDe = dateDe.substring(0, 10);
+      $scope.dataAte = new Date($scope.dataAte);
+      var dateAte = $scope.dataAte.toLocaleString();
+      dateAte = dateAte.substring(0, 10);
 
-			     $http.get(BASE_URL +'/relatorios?familia_id='+id_familia+'&dataDe='+consulta.de+'&dataAte='+consulta.ate+'&tipo='+consulta.tipo)
-			     	.success(function(data){
-			     		de(data);
-					$scope.labelsCategorias = data._embedded.relatorios[0].labelsCategoria;
-					$scope.dataCategorias   = data._embedded.relatorios[0].dataCategoria;
+      var consulta = {
+        de: dateDe,
+        ate: dateAte,
+        tipo: $scope.consulta.tipo
+      };
+
+      $http.get(BASE_URL + '/relatorios?familia_id=' + id_familia + '&dataDe=' + consulta.de + '&dataAte=' + consulta.ate + '&tipo=' + consulta.tipo)
+        .success(function(data) {
+					de(data);
+					$scope.labelsCategorias = data._embedded.relatorios[0].labelsCategoria.replace("\'", "\"");
+
+					var labels = data._embedded.relatorios[0].labelsCategoria.substring(1);
+
+          $scope.dataCategorias = [38,1];
+					$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+					$scope.data = [300, 500, 100];
+          $scope.showCharts = !$scope.showCharts;
+					de($scope.labelsCategorias);
 					de($scope.dataCategorias);
-					$scope.labels = data._embedded.relatorios[0].labelsCategoria;
-              	    $scope.showCharts       = !$scope.showCharts;
+        }).error(function(error) {
+          de(error);
+        });
 
-			        
-
-			     	}).error(function(error){
-			     		de(error);
-			     });
-		
-			}
+    }
 
 
-			
-            $scope.data = [8,1,2];
-    	};
+
+    $scope.data = [8, 1, 2];
+  };
 
 })()
